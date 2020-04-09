7201dad626e6905d46319d40b2ace65166b28eee
Daniel Tadeu dos Santos <danieltadeu2@hotmail.com>
2020-04-08 23:44:15 -0300

    Inclusao do código no Github

---
+++ src/DevIO.App/ViewModels/EnderecoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.App.ViewModels
+{
+    public class EnderecoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Logradouro { get; set; }
+
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(8, ErrorMessage ="O campo {0} precisa ter {1} caracteres",MinimumLength = 8)]
+        public string Cep { get; set; }
+        [HiddenInput]
+        public Guid FornecedorId { get; set; }
+    }
+}
